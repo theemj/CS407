068ca4accbf7b787bbb99f18fff1409e9f562fb4
KamiyaSamurai <aawood@email.meredith.edu>
2016-03-28 14:56:48 -0400

    Latest version

--- addQuestion.php
+++ addQuestion.php
@@ -1,6 +1,8 @@
 <?php
 //addQuestion.php
-//Emily Johnson modified by Alicia Wood
+//Emily Johnson 
+//modified by Alicia Wood
+//edited by Maryam Ahmed
 //CS407
 //processes question POST data from ask_It.php
 //(which is one question and two answer choices)
@@ -9,7 +11,7 @@
 session_start();
 
 include("includeMe.php");				//php files to include
-include("openDBbatman.php");
+include("openDB.php");
 
 openDB();
 
@@ -23,13 +25,13 @@ openDB();
    $userID = '1234';					//fill in faux numbers until algorithm is finished for auto count
    $postID = '01';
    
-   $getID = "SELECT MAX(postID) FROM Question";
+   $getID = "SELECT MAX(postID) FROM Question;";
    
    $resultID = mysql_query($getID);						//get a table (last row of Question's table)
 	   
    if(noerror($resultID))
    {
-	   $postID = makeMax($resultID);					//get the actual number to be added to this knew post
+	   $postID = makeMax($resultID);					//get the actual number to be added to this new post
    }
 
    $query = "INSERT INTO Question SET "
@@ -44,12 +46,12 @@ openDB();
    if(noerrorquiet(mysql_query($query)))
    {
       echo "it worked!";
-      header("Location: home.php");
+      header("Location: my_Arguments.php");
    }
    else													//if there is some error sending row to table
    {
       echo "something's wrong";
-      //header("Location: ask_It.php"); maybe?
+      header("Location: ask_It.php");
    }
 ?>
 
@@ -82,20 +84,20 @@ openDB();
       <h1>Processing...</h1>
 
       <?php
-         echo "putting question ($questionText) and options ($answer1Text , $answer2Text) into table";
-	 echo "query is: $query";
-	 echo "<br>";
+		echo "putting question ($questionText) and options ($answer1Text , $answer2Text) into table";
+		echo "query is: $query";
+		echo "<br>";
 
-	//show all values of Question table
-	 $q = "SELECT * FROM Question;";		//create a query for mysql
-	 echo "about to do $q <br> \n";			//echo query
+		//show all values of Question table
+		$q = "SELECT * FROM Question;";		//create a query for mysql
+		echo "about to do $q <br> \n";			//echo query
 
-	 $result = mysql_query($q);			//send value of variable to database and get result
+		$result = mysql_query($q);			//send value of variable to database and get result
 
-	 if(noerror($result))
-	 {
-	    tabledump($result);				//print results of mysql query in table format if no errors
-	 }
+		if(noerror($result))
+		{
+			tabledump($result);				//print results of mysql query in table format if no errors
+		}
       ?>
    </body>
 </html>
