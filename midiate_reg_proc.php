ad5ad112ee9786d5341784952f8152d6076a1907
KamiyaSamurai <aawood@email.meredith.edu>
2016-04-06 16:33:18 -0400

    with User id added to table

--- midiate_reg_proc.php
+++ midiate_reg_proc.php
@@ -14,16 +14,17 @@
    $email = $_POST['email'];
    $password1 = $_POST['password'];
    $password2 = $_POST['passwordCheck'];
-   $emailAddress = $_POST['emailAddress'];
+
+   $userId = "234";								//change later!
 
    //Check to see if the email already exists in system (in other words, the email is already in use)
    $findEmail= "SELECT * FROM User WHERE email=$email;";
-   $result = mysql_query( $findUserName );
+   $result = mysql_query( $findEmail );
 
    if($result != null)
    {					 	 //if any records match the email, print notice to screen
       echo "<!DOCTYPE> <html> <body>";
-      echo "This user name already exists.";
+      echo "This email address is already in use.\n";
       echo "Return to <a href=\"midiate_reg.html\">registration</a> and try again.";
       echo "</body> </html>";
    }
@@ -32,7 +33,7 @@
       if( $password1 != $password2)
       {				   		//if any passwords do not match each other, print notice to screen
          echo "<!DOCTYPE> <html> <body>";
-         echo "Your passwords do not match.";
+         echo "Your passwords do not match.\n";
          echo "Return to <a href=\"midiate_reg.php\">registration</a> and try again.";
          echo "</body> </html>";
       }
@@ -41,8 +42,9 @@
 	//change this later so that after registration user gets an email to finish registration?
          $_SESSION['loggedIn'] = "yes";			//mark user as logged in
          $_SESSION['email'] = $email;
+         $_SESSION['meadUserId'] = $userId;
 
-	 $addAccount = "INSERT INTO User SET email = '$email', "
+	 $addAccount = "INSERT INTO User SET userID='$userId', email = '$email', "
 						. "password = '$password1';";
 	 mysql_query( $addAccount );
 
